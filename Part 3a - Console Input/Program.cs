08a36bfd036740bc7a95cfc760f03e7a724aae3a
YurBoyJonny <CheuJona853@GoTVDSB.ca>
2022-03-08 22:07:25 -0500

    Done

--- Part 3a - Console Input/Program.cs
+++ Part 3a - Console Input/Program.cs
@@ -1,55 +1,87 @@
 ﻿//Part 3a - Console Input
-
-
-
 using System;
+using System.Linq;
 
-namespace Part_3a___Console_Input
+namespace Part3aConsoleInput
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)             
         {
 
+            ///////////////////////////////////////////////////////////////////////// Greetings With System Date
+            Console.WriteLine("----Greetings With System Date----");
+            DateTime myDateTime = DateTime.Now;
+            string findYear = myDateTime.Year.ToString();
 
-            // Greetings With System Date
-            DateTime aDate = DateTime.Now;
-
-            string response;
-            Console.WriteLine("Greetings, today's date is: ");
-            Console.WriteLine(aDate.ToString("MM/dd/yyyy"));
-            Console.WriteLine("Is this correct?");
-            response = Console.ReadLine();
-            Console.WriteLine("You said: " + response);
+            int currentYear = Convert.ToInt32(findYear);
 
             string name;
             Console.WriteLine("What is your name?");
             name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name);
 
-            string age;
+            int userAge;
             Console.WriteLine("What is your age?");
-            age = Console.ReadLine();
-            Console.WriteLine("You are " + age + " years old!");
+            userAge = Convert.ToInt32(Console.ReadLine());
 
-            //string yearborn;
+            int yearBorn;
+            yearBorn = currentYear - userAge;
+            Console.WriteLine("Hello " + name + " you were born in " + yearBorn);
+            ///////////////////////////////////////////////////////////////////////// Adder 
+            Console.WriteLine("----Adder----");
+            int result;
+            int num1, num2, num3;
+            Console.WriteLine("Enter the first integer");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second integer");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third integer");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            result = num1 + num2 + num3;
+            Console.WriteLine("{0} + {1} + {2} = {3}", num1, num2, num3, result);
+            ///////////////////////////////////////////////////////////////////////// Distance
+            Console.WriteLine("----Distance----");
+            double firstDist, secondDist, thirdDist;
 
-            //Console.WriteLine("You were born in " + yearborn);
+            Console.WriteLine("Enter the first distance");
+            firstDist = Convert.ToDouble(Console.ReadLine());
 
-            // Adder 
+            Console.WriteLine("Enter the second distance");
+            secondDist = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine("Enter the third distance");
+            thirdDist = Convert.ToDouble(Console.ReadLine());
 
-            // Distance
+            double addition;
+            addition = firstDist + secondDist + thirdDist;
 
+            double division;
+            division = addition / 3;
 
-            // Hypotenuse
+            double average;
+            average = Math.Round(division, 2);
 
+            Console.WriteLine("The average distance is " + average + " km");
+            ///////////////////////////////////////////////////////////////////////// Hypotenuse
+            Console.WriteLine("----Hypotenuse----");
 
+            Console.WriteLine("Enter the length of the first leg");
+            Double leg1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the length of the second leg");
+            Double leg2 = Convert.ToDouble(Console.ReadLine());
+
+            Double addedLeg1 = leg1 * leg1;
+            Double addedLeg2 = leg2 * leg2;
+
+            Double legsTogether = addedLeg1 + addedLeg2;
+            Double hypotenuse = Math.Sqrt(legsTogether);
+            
+            Double roundedHypotenuse = Math.Round(hypotenuse, 2);
+            Console.WriteLine("The hypotenuse is " + roundedHypotenuse);
             Console.ReadKey();
+            
         }
     }
 }
 
-
-
-
+ 
